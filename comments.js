cc43d9551cd2eaf3ceeda5e77b73e19cf5c90914
Muhammad Younas <engr.younasbangash@gmail.com>
2024-01-28 09:26:35 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,26 +1,68 @@
-// create web server
-// 1. create server
-// 2. listen to requests
-// 3. handle requests
-// 4. send response
+// Create web server
+var express = require('express');
+var router = express.Router();
+var db = require('../models/db');
+var bodyParser = require('body-parser');
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
 
-// 1. create server
-// 2. listen to requests
-// 3. handle requests
-// 4. send response
-
-// 1. create server
-const express = require('express');
-
-// 2. listen to requests
-const app = express();
-
-// 3. handle requests
-app.get('/', function(req, res) {
-    res.send('Hello World');
+// Get comment
+router.get('/', function(req, res) {
+    if (req.session.user) {
+        var user = req.session.user;
+        var query = 'SELECT * FROM comments';
+        db.query(query, function(err, rows, fields) {
+            if (err) throw err;
+            res.render('comments', {title: 'Comments', rows: rows, user: user});
+        });
+    } else {
+        res.redirect('/login');
+    }
 });
 
-// 4. send response
-app.listen(3000, function() {
-    console.log('Server is running on port 3000');
+// Add comment
+router.post('/add', urlencodedParser, function(req, res) {
+    if (req.session.user) {
+        var user = req.session.user;
+        var comment = req.body.comment;
+        var query = 'INSERT INTO comments (comment) VALUES (?)';
+        db.query(query, [comment], function(err, rows, fields) {
+            if (err) throw err;
+            res.redirect('/comments');
+        });
+    } else {
+        res.redirect('/login');
+    }
 });
+
+// Edit comment
+router.post('/edit/:id', urlencodedParser, function(req, res) {
+    if (req.session.user) {
+        var user = req.session.user;
+        var id = req.params.id;
+        var comment = req.body.comment;
+        var query = 'UPDATE comments SET comment = ? WHERE id = ?';
+        db.query(query, [comment, id], function(err, rows, fields) {
+            if (err) throw err;
+            res.redirect('/comments');
+        });
+    } else {
+        res.redirect('/login');
+    }
+});
+
+// Delete comment
+router.get('/delete/:id', function(req, res) {
+    if (req.session.user) {
+        var user = req.session.user;
+        var id = req.params.id;
+        var query = 'DELETE FROM comments WHERE id = ?';
+        db.query(query, [id], function(err, rows, fields) {
+            if (err) throw err;
+            res.redirect('/comments');
+        });
+    } else {
+        res.redirect('/login');
+    }
+});
+
+module.exports = router;
